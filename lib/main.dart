a33405013d678ae507d8aaaee5ce8181a0a387a0
likaien927 <148830396+likaien996@users.noreply.github.com>
2024-06-23 22:40:46 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Food Delivery',
+      title: 'MEITUAN',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
